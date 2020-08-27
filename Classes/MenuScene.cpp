8ff523e11e492e32cebe2399a96a1aa8f4cb3a48
Nazar <hesab.k18@gmail.com>
2020-08-28 01:12:33 +0300

    Added good menu background

--- Classes/MenuScene.cpp
+++ Classes/MenuScene.cpp
@@ -1,60 +1,130 @@
 #include "MenuScene.h"
+#include<CCDirector.h>
 USING_NS_CC;
-
+MenuScene::MenuScene()
+{
+	screenSize = Director::getInstance()->getOpenGLView()->getFrameSize();
+	Schedule();
+}
+MenuScene::~MenuScene()
+{
+	Unschedule();
+}
 Scene* MenuScene::createScene()
 {
 	return MenuScene::create();
 }
-
 bool MenuScene::init()
 {
 	if (!Scene::init())
 	{
 		return false;
 	}
+	//Director::getInstance()->setDisplayStats(false);                    ////Is FPS and other debug information visible
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	Vector<MenuItem*> MenuItems;
 
-	ui::Button* button = ui::Button::create("images/ButtonNormal.jpg", "images/ButtonSelected.jpg", "images/ButtonDisabled.jpg");
-	if (button == nullptr)
-	{
-		Utils::problemLoading("'fonts/Marker Felt.ttf'");
-		return false;
-	}
-	else
-	{
-		//button->setScale(4.f);
-		button->setContentSize(Size(250,50));
-		float x = visibleSize.width / 2;
-		float y = visibleSize.height / 2;
-		button->setPosition(Vec2(x, y));
-		button->setTitleText("Start Game");
-		button->setTitleFontSize(12.f);
-		button->addTouchEventListener([&](Ref* sender, ui::Widget::TouchEventType type) {
-			switch (type)
-			{
-			case ui::Widget::TouchEventType::BEGAN:
-				break;
-			case ui::Widget::TouchEventType::ENDED:
-				Director::getInstance()->replaceScene(HelloWorld::createScene());
-				break;
-			default:
-				break;
-			}
-			});
+	Dl = Label::createWithSystemFont("_", "Consolas", 32);
+	Dl->setPosition(200, 250);
+	_point = Sprite::create("images/point.png");
+	_point->setScale(2);
+	this->addChild(_point);
+	this->addChild(Dl);
 
-		this->addChild(button);
-	}
+	MenuItems.pushBack(MenuItemFont::create(StringUtils::toString(screenSize.width) + " : " + StringUtils::toString(screenSize.height),
+		[&](Ref* sender)
+		{
+			_point->setPosition(_point->getPosition().x - 10, _point->getPosition().y);
+			Dl->setString("Coord : x= " + StringUtils::toString(_point->getPosition().x) + " | y= " + StringUtils::toString(_point->getPosition().y));
+			//Director::getInstance()->replaceScene(HelloWorld::createScene());
+		}));
+	MenuItems.back()->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y + 30.f);
+	MenuItems.pushBack(MenuItemFont::create("Game Creators",
+		[&](Ref* sender)
+		{
+			_point->setPosition(_point->getPosition().x + 10, _point->getPosition().y);
+			Dl->setString("Coord : x= " + StringUtils::toString(_point->getPosition().x) + " | y= " + StringUtils::toString(_point->getPosition().y));
+			//Director::getInstance()->replaceScene(TransitionFade::create(0.5f, HelloWorld::createScene()));
+		}));
+	MenuItems.back()->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y);
+	MenuItems.pushBack(MenuItemFont::create("Exit Game",
+		[&](Ref* sender)
+		{
+			_point->setPosition(_point->getPosition().x, _point->getPosition().y + 10);
+			Dl->setString("Coord : x= " + StringUtils::toString(_point->getPosition().x) + " | y= " + StringUtils::toString(_point->getPosition().y));
+		}));
+	MenuItems.back()->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y - 30.f);
+	MenuItems.pushBack(MenuItemFont::create("Exit Game",
+		[&](Ref* sender)
+		{
+			//Director::getInstance()->end();
+			_point->setPosition(_point->getPosition().x, _point->getPosition().y - 10);
+			Dl->setString("Coord : x= " + StringUtils::toString(_point->getPosition().x) + " | y= " + StringUtils::toString(_point->getPosition().y));
+		}));
+	MenuItems.back()->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y - 60.f);
+	Menu* menu = Menu::createWithArray(MenuItems);
+	menu->setPosition(0, 0);
+	this->addChild(menu, 1);
+	BackgroundImagesInit();
+
+
+	_point->setPosition(10.f, 10.f + origin.y);
+
 	return true;
 }
 
-void MenuScene::gameCloseCallback(Ref* pSender)
+void MenuScene::GameCloseCallback(Ref* pSender)const
 {
-	//Close the cocos2d-x game scene and quit the application
 	Director::getInstance()->end();
-	/*To navigate back to native iOS screen(if present) without quitting the application
-	,do not use Director::getInstance()->end() as given above,instead trigger a custom
-	event created in RootViewController.mm as below*/
-	//EventCustom customEndEvent("game_scene_close_event");
-	//_eventDispatcher->dispatchEvent(&customEndEvent);
 }
+void MenuScene::GameStartCallback(Ref* sender)const
+{
+	Director::getInstance()->replaceScene(HelloWorld::createScene());
+}
+
+void MenuScene::Schedule()
+{
+	cocos2d::Director::getInstance()->getScheduler()->schedule(CC_CALLBACK_1(MenuScene::Update, this), this, 1.0f / 60, false, "menuScheduler");
+}
+
+void MenuScene::Unschedule()
+{
+	cocos2d::Director::getInstance()->getScheduler()->unschedule("menuScheduler", this);
+}
+
+void MenuScene::Update(const float dt)
+{
+	if (_backgroundImage == NULL || _backgroundImage == nullptr)
+	{
+		return;
+	}
+	static float t = 0.0f;
+	t += dt;
+	const auto tx = 100 * cosf(t / 10);
+	const auto ty = 100 * sinf(t / 10);
+	Size visibleSize = Director::getInstance()->getWinSize();
+	_backgroundImage->setTextureRect(cocos2d::Rect(tx, ty, screenSize.width, screenSize.height));
+}
+
+void MenuScene::BackgroundImagesInit()
+{
+	Size visibleSize = Director::getInstance()->getWinSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	_backgroundImage = Sprite::create("images/bg.png");
+	_backgroundImage->setPosition(0, 0);
+	Texture2D::TexParams params;
+	params.minFilter = backend::SamplerFilter::NEAREST;
+	params.magFilter = backend::SamplerFilter::NEAREST;
+	params.sAddressMode = backend::SamplerAddressMode::REPEAT;
+	params.tAddressMode = backend::SamplerAddressMode::REPEAT;
+	_backgroundImage->getTexture()->setTexParameters(params);
+	_backgroundImage->setTextureRect(cocos2d::Rect(0, 0, screenSize.width, screenSize.height));
+	this->addChild(_backgroundImage, -1);
+}
+
+void MenuScene::BackgroundImagesRelease()const
+{
+}
+
+
--- Classes/MenuScene.h
+++ Classes/MenuScene.h
@@ -1,16 +1,29 @@
 #ifndef PROJ_ANDROID_MENUSCENE_H
 #define PROJ_ANDROID_MENUSCENE_H
 #include <cocos2d.h>
+#include <math.h>
 #include "HelloWorldScene.h"
 #include "Utils.h"
 #include <ui/CocosGUI.h>
 class MenuScene :public cocos2d::Scene
 {
-public:
-	static cocos2d::Scene* createScene();
+private:
+	cocos2d::Size screenSize;
+	cocos2d::Sprite* _backgroundImage;
+	cocos2d::Sprite* _point;
+	cocos2d::Label* Dl;
 	virtual bool init();
-	void gameCloseCallback(cocos2d::Ref* pSender);
-	// implement the "static create()" method manually
+	void GameCloseCallback(cocos2d::Ref* pSender)const;
+	void GameStartCallback(cocos2d::Ref* pSender)const;
+	void Schedule();
+	void Unschedule();
+	void Update(const float dt);
+	void BackgroundImagesInit();
+	void BackgroundImagesRelease()const;
+public:
+	MenuScene();
+	~MenuScene();
+	static cocos2d::Scene* createScene();
 	CREATE_FUNC(MenuScene);
 };
 #endif //PROJ_ANDROID_MENUSCENE_H
